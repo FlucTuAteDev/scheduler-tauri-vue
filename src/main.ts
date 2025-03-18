ad7bba1306e59f5e302ead3b70e4e6dd183a9d43
Guczi Ádám <gucziadamlaszlo@gmail.com>
2025-03-18 21:24:23 +0100

    Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { createApp } from "vue";
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify';
+
+// Components
 import App from "./App.vue";
+
+// Devtools
 import { devtools } from "@vue/devtools";
 
 if (process.env.NODE_ENV === "development") {
 	devtools.connect();
 }
 
-createApp(App).mount("#app");
+const vuetify = createVuetify();
+
+createApp(App).use(vuetify).mount("#app");
