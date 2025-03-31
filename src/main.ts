700f3754a666a19d86d5dfd5955e6fcae05d129f
Guczi Ádám <gucziadamlaszlo@gmail.com>
2025-04-01 01:03:34 +0200

    Basic auto updater

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,41 @@ import App from "./App.vue";
 // Devtools
 import { devtools } from "@vue/devtools";
 
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+
 if (process.env.NODE_ENV === "development") {
 	devtools.connect();
 }
 
 createApp(App).use(vuetify).mount("#app");
+
+const update = await check();
+if (update) {
+	console.log(
+		`found update ${update.version} from ${update.date} with notes ${update.body}`,
+	);
+	let downloaded = 0;
+	let contentLength = 0;
+	// alternatively we could also call update.download() and update.install() separately
+	await update.downloadAndInstall(event => {
+		switch (event.event) {
+			case "Started":
+				contentLength = event.data.contentLength ?? 0;
+				console.log(
+					`started downloading ${event.data.contentLength} bytes`,
+				);
+				break;
+			case "Progress":
+				downloaded += event.data.chunkLength;
+				console.log(`downloaded ${downloaded} from ${contentLength}`);
+				break;
+			case "Finished":
+				console.log("download finished");
+				break;
+		}
+	});
+
+	console.log("update installed");
+	await relaunch();
+}
